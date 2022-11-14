52573d82c2442d483521105d5d076c8e6ad61c72
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2022-11-14 19:35:31 +0100

    Ik heb de stractuur van de project gemaakt

---
+++ webApp.js
@@ -0,0 +1,30 @@
+
+ 
+
+
+var antworden = [];
+
+var vragen = [];
+
+function hideTheStart(){
+
+    document.getElementById('start').style.display='none'
+    document.getElementById('1').style.display='block'
+
+}
+
+
+function answerEens(){
+
+}
+
+function answerOnEens(){
+    
+}
+
+function answerGeen(){
+    
+}
+
+
+console.log(subjects[0].parties[1]);
