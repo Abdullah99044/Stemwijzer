5d3ed21172b4283df17c1ba5bc792e6109be020c
Abdullah99044 <89779550+Abdullah99044@users.noreply.github.com>
2022-11-18 09:24:19 +0100

    nieuwe javascript functies

--- webApp.js
+++ webApp.js
@@ -1,30 +1,106 @@
 
  
 
+// ################################################
+ 
+// Partijen toevoegen aan partijen array
+
+// ################################################
+
+var partijen = [];
+
+
+for (let x in parties) {
+    partijen.push(parties[x].name);
+}
+
+console.log(partijen);
+
+
+// ################################################
+ 
+//  Statments telen
+
+// ################################################
+
+var statements = [];
+
+let num = 1;
+
+for (let x in subjects ) {
+    statements.push(num);
+    num++;
+}
+ 
+ 
+
+// ################################################
+
 
 var antworden = [];
 
-var vragen = [];
+
+ 
+
+var vragenNummer = 0;
 
 function hideTheStart(){
 
     document.getElementById('start').style.display='none'
-    document.getElementById('1').style.display='block'
+
+    var statmentNummer = statements[vragenNummer];
+
+    if(statmentNummer < 31){
+
+
+        
+        var title = subjects[vragenNummer].title;
+        var statment = subjects[vragenNummer].statement;
+
+
+        // satments nummer
+        document.getElementById('statmentNum').innerHTML =  statmentNummer + " / " + statements.slice(-1);
+
+        document.getElementById('1').style.display='block'
+
+        document.getElementById('title').innerHTML    = title
+        document.getElementById('statment').innerHTML = statment
+
+    }else{
+
+        document.getElementById('1').style.display='none'
+        document.getElementById('statmentNum').style.display='none'
+        document.getElementById('resultButton').style.display='block'
+    }
+    
+    vragenNummer++;
 
 }
 
 
 function answerEens(){
-
+    antworden.push("eens");
+    return hideTheStart();
+    
 }
 
 function answerOnEens(){
-    
+    antworden.push("oneens");
+    return hideTheStart();
 }
 
 function answerGeen(){
+    antworden.push("geen");
+    return hideTheStart();
     
 }
 
+console.log(antworden);
 
-console.log(subjects[0].parties[1]);
+
+
+
+
+
+  
+ 
